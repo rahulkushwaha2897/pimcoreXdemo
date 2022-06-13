2d6c6a36efac9768b5bf81392288509f44e22d28
rahulkushwaha2897 <76255247+rahulkushwaha2897@users.noreply.github.com>
2022-06-13 15:51:50 +0530

    Update 1_unlink.sh

--- .platform/hooks/postdeploy/1_unlink.sh
+++ .platform/hooks/postdeploy/1_unlink.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-declare -a links=("/var/www/html/web/var", "/var/www/html/var/config", "/var/www/html/var/versions", "/var/www/html/var/tmp", "/var/www/html/var/classes")
+declare -a links=("/var/www/html/public/var", "/var/www/html/var/config", "/var/www/html/var/versions", "/var/www/html/var/tmp", "/var/www/html/var/classes")
 
 
 for i in "${links[@]}"
