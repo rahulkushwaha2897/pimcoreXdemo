7aa965ff15cbd8df73e16bb684701ea7c0a0d49c
rahulkushwaha2897 <76255247+rahulkushwaha2897@users.noreply.github.com>
2022-06-14 15:32:33 +0530

    Update 4_permission.sh

--- .platform/hooks/postdeploy/4_permission.sh
+++ .platform/hooks/postdeploy/4_permission.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-cat /etc/passwd | grep dev
-if [ "$?" != "0" ]; then
-        useradd dev
-fi
+#cat /etc/passwd | grep dev
+#if [ "$?" != "0" ]; then
+ #       useradd dev
+#fi
 
 usermod -a -G webapp dev
 
 cd /var/www/html
 sudo chown -R webapp.webapp .
-sudo find pimcore -type d -exec chmod 750 {} \;
-sudo find pimcore -type f -exec chmod 640 {} \;
+#sudo find pimcore -type d -exec chmod 750 {} \;
+#sudo find pimcore -type f -exec chmod 640 {} \;
 sudo find src -type f -exec chmod 640 {} \;
 sudo find src -type d -exec chmod 750 {} \;
-sudo find app -type f -exec chmod 640 {} \;
-sudo find app -type d -exec chmod 750 {} \;
+#sudo find app -type f -exec chmod 640 {} \;
+#sudo find app -type d -exec chmod 750 {} \;
 sudo find vendor -type d -exec chmod 750 {} \;
 sudo find vendor -type f -exec chmod 640 {} \;
-sudo find web -type d -exec chmod 750 {} \;
-sudo find web -type f -exec chmod 640 {} \;
+#sudo find web -type d -exec chmod 750 {} \;
+#sudo find web -type f -exec chmod 640 {} \;
 chown -R webapp:webapp /efs/storage/var/classes
 chmod -R 0775 /efs/storage/var/classes
