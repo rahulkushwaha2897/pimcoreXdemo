1d8f792ce3f4c12ba5b3f4f097a945adfc65cb3e
rahulkushwaha2897 <76255247+rahulkushwaha2897@users.noreply.github.com>
2022-06-15 16:56:02 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ php bin/console cache:clear
 sudo chmod -Rf 777 .
 sudo rm -rf var/sessions/*
 php bin/console assets:install --symlink --relative
-sudo chown -R www-data:www-data .
+sudo chown -R webapp:webapp .
 sudo chmod -Rf 777 .
 php bin/console pimcore:cache:clear
