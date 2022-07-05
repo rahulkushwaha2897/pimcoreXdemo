ca8e8abfcb8cc28d10d8519bca0faecd20d5b4c2
rahulkushwaha2897 <76255247+rahulkushwaha2897@users.noreply.github.com>
2022-07-05 16:57:10 +0530

    Update 2_linking.sh

--- .platform/hooks/postdeploy/2_linking.sh
+++ .platform/hooks/postdeploy/2_linking.sh
@@ -6,20 +6,20 @@ cd  /var/www/html/var
 mv tmp old-tmp
 
 mv classes/* /mnt/efs/var/classes/ 
-mv  /var/www/html/var/config/* /mnt/efs/var/config/
+#mv  config/* /mnt/efs/var/config/
 #linking var directory with efs storage directory
 #linking /var/www/html/var/* files
 rm -rf classes config versions
-ln -s /efs/var/classes .
-ln -s /efs/var/config .
-ln -s /efs/var/versions .
-ln -s /efs/var/tmp .
+ln -s /mnt/efs/var/classes .
+ln -s /mnt/efs/var/config .
+ln -s /mnt/efs/var/versions .
+ln -s /mnt/efs/var/tmp .
 #ln -s /efs/storage/var/recyclebin .
 
 #linking /var/www/html/web/var directory with efs storage directory
 cd  /var/www/html/public
 rm -rf var
-ln -s /efs/web-var  var 
+ln -s /mnt/efs/web-var  var 
 
 cd /var/www/html
 #if [ -d "builds" ]
