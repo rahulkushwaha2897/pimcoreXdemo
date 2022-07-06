d43cfc27dc84a5df11a3ac466b4745dbe11bfd3f
rahulkushwaha2897 <76255247+rahulkushwaha2897@users.noreply.github.com>
2022-07-06 15:20:30 +0530

    Update 2_linking.sh

--- .platform/hooks/postdeploy/2_linking.sh
+++ .platform/hooks/postdeploy/2_linking.sh
@@ -4,23 +4,23 @@
 yum install stunnel -y
 cd  /var/www/html/var
 mv tmp old-tmp
-mv classes/* /mnt/efs/var/classes/ 
-mv config/* /mnt/efs/var/config/
-mv versions/* /mnt/efs/var/versions/
-ll /mnt/efs/var/
+mv classes/* /efs/var/classes/ 
+mv config/* /efs/var/config/
+mv versions/* /efs/var/versions/
+#ll /efs/var/
 #linking var directory with efs storage directory
 #linking /var/www/html/var/* files
 rm -rf classes config versions
-ln -s /mnt/efs/var/classes .
-ln -s /mnt/efs/var/config .
-ln -s /mnt/efs/var/versions .
-ln -s /mnt/efs/var/tmp .
+ln -s /efs/var/classes .
+ln -s /efs/var/config .
+ln -s /efs/var/versions .
+ln -s /efs/var/tmp .
 #ln -s /efs/storage/var/recyclebin .
 
 #linking /var/www/html/web/var directory with efs storage directory
 cd  /var/www/html/public
 rm -rf var
-ln -s /mnt/efs/web-var  var 
+ln -s /efs/web-var  var 
 
 cd /var/www/html
 #if [ -d "builds" ]
