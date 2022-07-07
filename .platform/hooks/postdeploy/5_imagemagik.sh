cf6de163549c7445a454d756ec2c7f34550f51ca
rahulkushwaha2897 <76255247+rahulkushwaha2897@users.noreply.github.com>
2022-07-07 15:31:56 +0530

    Update 5_imagemagik.sh

--- .platform/hooks/postdeploy/5_imagemagik.sh
+++ .platform/hooks/postdeploy/5_imagemagik.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 cd /home/ec2-user/
-sudo curl -sS https://getcomposer.org/installer | sudo php
-sudo mv composer.phar /usr/bin/composer
+sudo curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
+sudo chmod +x /usr/local/bin/composer
+sudo composer.phar self-update 
 sudo composer.phar self-update --2.2
 yum install libjpeg-turbo.x86_64 libjpeg-turbo-devel.x86_64 libjpeg-turbo-static.x86_64 libjpeg-turbo-utils.x86_64 openjpeg.x86_64 openjpeg-devel.x86_64 openjpeg-libs.x86_64 turbojpeg.x86_64 turbojpeg-devel.x86_64 gd.x86_64 jasper.x86_64 -y
 yum install inkscape.x86_64 inkscape-view.x86_64 -y
