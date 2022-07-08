176b89c6c6ebb2859ccff0bcbe5b7180a9e94aa7
rahulkushwaha2897 <76255247+rahulkushwaha2897@users.noreply.github.com>
2022-07-08 14:59:57 +0530

    Update 5_imagemagik.sh

--- .platform/hooks/postdeploy/5_imagemagik.sh
+++ .platform/hooks/postdeploy/5_imagemagik.sh
@@ -16,4 +16,21 @@ yum install java-1.8.0-openjdk-headless.x86_64 -y
 
 cd /var/www/html
 sudo composer install
+
+cd /var/www/html/
+#cd builds/
+#mv * /efs/storage/var/classes/
+#cd /var/www/html/
+#rm -rf /var/www/html/builds
+#sudo php -d memory_limit=-1 composer install
+sudo chown -R webapp:webapp *
+sudo -u webapp php bin/console cache:clear
+sudo -u webapp php bin/console cache:warmup
+sudo php bin/console pimcore:deployment:classes-rebuild -d -c -q
+sudo -u webapp php bin/console assets:install 
+sudo chown -R webapp:webapp *
+sudo chmod -R 0775 *
+sudo service httpd restart 
+sudo service php-fpm restart
+
 ###Deployment Done
