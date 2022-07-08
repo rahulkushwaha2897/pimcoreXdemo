d335159f2940fe725e4d74bfb9e1361b53adb158
rahulkushwaha2897 <76255247+rahulkushwaha2897@users.noreply.github.com>
2022-07-08 12:19:06 +0530

    Update 2_linking.sh

--- .platform/hooks/postdeploy/2_linking.sh
+++ .platform/hooks/postdeploy/2_linking.sh
@@ -3,14 +3,12 @@
 #if [ $env == "Development" ];then
 yum install stunnel -y
 cd  /var/www/html/var
-mv tmp old-tmp
-mv classes/* /efs/storage/var/classes/ 
+mv classes/* /efs/storage/var/classes/
 mv config/* /efs/storage/var/config/
-mv versions/* /efs/storage/var/versions/
-#ll /efs/var/
+
 #linking var directory with efs storage directory
 #linking /var/www/html/var/* files
-rm -rf classes config  versions
+rm -rf classes config versions
 ln -s /efs/storage/var/classes .
 ln -s /efs/storage/var/config .
 ln -s /efs/storage/var/versions .
@@ -18,7 +16,7 @@ ln -s /efs/storage/var/tmp .
 #ln -s /efs/storage/var/recyclebin .
 
 #linking /var/www/html/web/var directory with efs storage directory
-cd  /var/www/html/public
+cd  /var/www/html/
 rm -rf var
 ln -s /efs/storage/web-var  var 
 
@@ -26,4 +24,4 @@ cd /var/www/html
 #if [ -d "builds" ]
 #then
 #echo "Copying  build folders and files to classes"
-#sudo cp -rf builds/* var/classes/
+sudo cp -rf builds/* var/classes/
