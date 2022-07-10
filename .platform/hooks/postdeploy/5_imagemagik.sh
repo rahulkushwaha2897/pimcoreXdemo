1cf515fd4a9ee3d4301d692ae3560e116972d88c
rahulkushwaha2897 <76255247+rahulkushwaha2897@users.noreply.github.com>
2022-07-10 15:54:58 +0530

    Update 5_imagemagik.sh

--- .platform/hooks/postdeploy/5_imagemagik.sh
+++ .platform/hooks/postdeploy/5_imagemagik.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-cd /home/ec2-user/
+cd /var/www/html/
 yum install libjpeg-turbo.x86_64 libjpeg-turbo-devel.x86_64 libjpeg-turbo-static.x86_64 libjpeg-turbo-utils.x86_64 openjpeg.x86_64 openjpeg-devel.x86_64 openjpeg-libs.x86_64 turbojpeg.x86_64 turbojpeg-devel.x86_64 gd.x86_64 jasper.x86_64 -y
 yum install inkscape.x86_64 inkscape-view.x86_64 -y
 yum install libtiff.x86_64 libtiff-static.x86_64 -y
 yum install libpng.x86_64 libpng-devel.x86_64 libpng-static.x86_64 pngcrush.x86_64 -y
 yum install lcms2-utils.x86_64 lcms2.x86_64 lcms2-devel.x86_64 -y
 yum install librsvg2-devel.x86_64 librsvg2-tools.x86_64 librsvg2.x86_64 -y
-yum install python3-pip.noarch -y
-pip3 install html2text
 yum install java-1.8.0-openjdk-headless.x86_64 -y
 
 
@@ -16,16 +14,17 @@ yum install java-1.8.0-openjdk-headless.x86_64 -y
 
 
 cp /usr/bin/composer.phar /usr/bin/composer
-sudo composer self-update --2
-sudo composer update ocramius/package-versions
+
+#sudo composer update ocramius/package-versions
 #cd /var/www/html/
 #cd builds/
 #mv * /efs/storage/var/classes/
 cd /var/www/html/
 
-#sudo composer self-update
+
 #rm -rf /var/www/html/builds
 sudo php -d memory_limit=-1 /usr/bin/composer.phar install
+sudo composer.phar self-update
 sudo chown -R webapp:webapp *
 sudo -u webapp php bin/console cache:clear
 sudo -u webapp php bin/console cache:warmup
