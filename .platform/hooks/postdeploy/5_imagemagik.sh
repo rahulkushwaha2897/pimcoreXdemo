f755c794d8256be8aa17ba2545f3d3603dc066e0
rahulkushwaha2897 <76255247+rahulkushwaha2897@users.noreply.github.com>
2022-07-14 13:25:58 +0530

    Update 5_imagemagik.sh

--- .platform/hooks/postdeploy/5_imagemagik.sh
+++ .platform/hooks/postdeploy/5_imagemagik.sh
@@ -10,14 +10,14 @@ yum install librsvg2-devel.x86_64 librsvg2-tools.x86_64 librsvg2.x86_64 -y
 yum install python3-pip.noarch -y
 pip3 install html2text
     sudo yum install java-1.8.0-openjdk-headless.x86_64 -y
-   sudo  yum update -y
+   #sudo  yum update -y
   #  sudo yum -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
   #  sudo yum -y install https://rpms.remirepo.net/enterprise/remi-release-7.rpm
   #  sudo yum makecache
   #  sudo yum -y install yum-utils
   #  sudo yum clean metadata
-    yum install php80-php-redis -y
-    yum install php-redis -y
+  #  yum install php80-php-redis -y
+   # yum install php-redis -y
   
 
 #printf "/usr/local" |pecl install imagick 
